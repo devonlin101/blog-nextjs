cc246c01eab739ae03b1fbf9e58866b0654b1c21
devonlin101 <devonlin101@gmail.com>
2024-05-25 12:47:42 -0400

    added google adsense

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
+import GoogleAdsense from './GoogleAsense'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
@@ -9,6 +10,7 @@ import SearchButton from './SearchButton'
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
+      <GoogleAdsense pId="5229295142210026" />
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
